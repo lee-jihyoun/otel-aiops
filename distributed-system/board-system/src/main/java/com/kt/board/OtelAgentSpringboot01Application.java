b67a8398885b2f0af592dc9b5b075e4145261991
kimsc1218 <kimsc1218@gmail.com>
2024-08-09 01:40:02 +0900

    2024.08.09 - tomcat에서 war 파일 배포를 위한 설정 추가

--- distributed-system/board-system/src/main/java/com/kt/board/OtelAgentSpringboot01Application.java
+++ distributed-system/board-system/src/main/java/com/kt/board/OtelAgentSpringboot01Application.java
@@ -2,12 +2,20 @@ package com.kt.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OtelAgentSpringboot01Application {
+public class OtelAgentSpringboot01Application extends SpringBootServletInitializer {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(OtelAgentSpringboot01Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return super.configure(application);
 	}
 
 }
