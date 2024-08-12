809f845f5f3931433a02d4e29ed1a1145cc5e7aa
lee-jihyoun <lee.jihyoun7@gmail.com>
2024-08-12 16:59:33 +0900

    2024.08.12 - processor 수정

--- distributed-system/board-system/src/main/java/com/kt/board/OtelAgentSpringboot01Application.java
+++ distributed-system/board-system/src/main/java/com/kt/board/OtelAgentSpringboot01Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OtelAgentSpringboot01Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(OtelAgentSpringboot01Application.class, args);
 	}
+
 }
