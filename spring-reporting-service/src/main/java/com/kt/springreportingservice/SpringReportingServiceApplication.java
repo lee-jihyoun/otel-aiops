1ac9c2c994f414524e99cef5d07a0ae779474741
kimsc1218 <kimsc1218@gmail.com>
2024-09-12 00:21:36 +0900

    2024.09.12 - war파일용 브런치 생성

--- spring-reporting-service/src/main/java/com/kt/springreportingservice/SpringReportingServiceApplication.java
+++ spring-reporting-service/src/main/java/com/kt/springreportingservice/SpringReportingServiceApplication.java
@@ -2,14 +2,21 @@ package com.kt.springreportingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class SpringReportingServiceApplication {
+public class SpringReportingServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReportingServiceApplication.class, args);
 	}
 
+	//war 배포
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringReportingServiceApplication.class);
+	}
 }
--- spring-reporting-service/src/main/java/com/kt/springreportingservice/report/scheduler/ErrorReportScheduler.java
+++ spring-reporting-service/src/main/java/com/kt/springreportingservice/report/scheduler/ErrorReportScheduler.java
@@ -1,67 +1,67 @@
-package com.kt.springreportingservice.report.scheduler;
-
-import com.kt.springreportingservice.report.domain.ErrorReport;
-import com.kt.springreportingservice.report.domain.MailSendInfo;
-import com.kt.springreportingservice.report.domain.ServiceInfo;
-import com.kt.springreportingservice.report.domain.UserInfo;
-import com.kt.springreportingservice.report.repository.ErrorReportRepository;
-import com.kt.springreportingservice.report.repository.MailSendInfoRepository;
-import com.kt.springreportingservice.report.repository.UserInfoRepository;
-import com.kt.springreportingservice.report.service.MailService;
-import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Map;
-
-
-@AllArgsConstructor
-@Component
-public class ErrorReportScheduler {
-
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    private ErrorReportRepository errorReportRepository;
-
-    private UserInfoRepository userInfoRepository;
-
-    private MailSendInfoRepository mailSendInfoRepository;
-
-    private MailService mailService;
-
-    @Transactional
-    @Scheduled(fixedRate = 60000) // 1분마다 실행
-    public void checkErrorReports() {
-
-        logger.info("### ErrorReportScheduler started");
-        List<ErrorReport> errorReports = errorReportRepository.findByErrorReportSendYn("N"); // 'N'인 데이터만 조회
-
-        if (!errorReports.isEmpty()) {
-            for (ErrorReport errorReport : errorReports) {
-                ServiceInfo serviceInfo = errorReport.getServiceInfo(); // ServiceInfo 객체를 가져옴
-                String serviceCode = serviceInfo.getServiceCode(); // ServiceInfo에서 serviceCode 가져옴
-                // user_info 테이블에서 service_code로 사용자 목록 조회
-                List<UserInfo> users = userInfoRepository.findByServiceInfo(serviceInfo);
-                Map<String,String> mailForm =  mailService.createMailForm(errorReport);
-                for (UserInfo user : users) {
-                    //메일발송
-                    mailService.sendEmail(user.getEmail(), mailForm.get("mailTitle"), mailForm.get("mailContent"));
-                    
-                    MailSendInfo mailSendInfo = new MailSendInfo();
-                    mailSendInfo.setReceiverEmail(user.getEmail());
-                    mailSendInfo.setErrorReport(errorReport); // ErrorReport 객체를 직접 설정
-                    mailSendInfo.setServiceCode(serviceCode); // serviceCode 설정
-                    mailSendInfo.setIsMailSent("Y");
-                    mailSendInfoRepository.save(mailSendInfo);
-                }
-                errorReport.setErrorReportSendYn("Y");
-                errorReportRepository.save(errorReport);
-            }
-        }
-        logger.info("### ErrorReportScheduler ended");
-    }
-}
+//package com.kt.springreportingservice.report.scheduler;
+//
+//import com.kt.springreportingservice.report.domain.ErrorReport;
+//import com.kt.springreportingservice.report.domain.MailSendInfo;
+//import com.kt.springreportingservice.report.domain.ServiceInfo;
+//import com.kt.springreportingservice.report.domain.UserInfo;
+//import com.kt.springreportingservice.report.repository.ErrorReportRepository;
+//import com.kt.springreportingservice.report.repository.MailSendInfoRepository;
+//import com.kt.springreportingservice.report.repository.UserInfoRepository;
+//import com.kt.springreportingservice.report.service.MailService;
+//import lombok.AllArgsConstructor;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//
+//@AllArgsConstructor
+//@Component
+//public class ErrorReportScheduler {
+//
+//    private final Logger logger = LoggerFactory.getLogger(getClass());
+//
+//    private ErrorReportRepository errorReportRepository;
+//
+//    private UserInfoRepository userInfoRepository;
+//
+//    private MailSendInfoRepository mailSendInfoRepository;
+//
+//    private MailService mailService;
+//
+//    @Transactional
+//    @Scheduled(fixedRate = 60000) // 1분마다 실행
+//    public void checkErrorReports() {
+//
+//        logger.info("### ErrorReportScheduler started");
+//        List<ErrorReport> errorReports = errorReportRepository.findByErrorReportSendYn("N"); // 'N'인 데이터만 조회
+//
+//        if (!errorReports.isEmpty()) {
+//            for (ErrorReport errorReport : errorReports) {
+//                ServiceInfo serviceInfo = errorReport.getServiceInfo(); // ServiceInfo 객체를 가져옴
+//                String serviceCode = serviceInfo.getServiceCode(); // ServiceInfo에서 serviceCode 가져옴
+//                // user_info 테이블에서 service_code로 사용자 목록 조회
+//                List<UserInfo> users = userInfoRepository.findByServiceInfo(serviceInfo);
+//                Map<String,String> mailForm =  mailService.createMailForm(errorReport);
+//                for (UserInfo user : users) {
+//                    //메일발송
+//                    mailService.sendEmail(user.getEmail(), mailForm.get("mailTitle"), mailForm.get("mailContent"));
+//
+//                    MailSendInfo mailSendInfo = new MailSendInfo();
+//                    mailSendInfo.setReceiverEmail(user.getEmail());
+//                    mailSendInfo.setErrorReport(errorReport); // ErrorReport 객체를 직접 설정
+//                    mailSendInfo.setServiceCode(serviceCode); // serviceCode 설정
+//                    mailSendInfo.setIsMailSent("Y");
+//                    mailSendInfoRepository.save(mailSendInfo);
+//                }
+//                errorReport.setErrorReportSendYn("Y");
+//                errorReportRepository.save(errorReport);
+//            }
+//        }
+//        logger.info("### ErrorReportScheduler ended");
+//    }
+//}
