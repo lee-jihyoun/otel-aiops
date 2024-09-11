19ac8e3cefeacf771524de67b9d9989d8d5a4898
kimsc1218 <kimsc1218@gmail.com>
2024-09-11 21:01:26 +0900

    2024.09.11 - otel http listener에서 파일 삭제 스케줄러 추가 - 스프링 리포트 스케줄러에서 에러발생 케이스 추가 - 리눅스에서 사용하는 명령어들 및 설정들 업로드

---
+++ spring-reporting-service/src/main/java/com/kt/springreportingservice/report/repository/ErrorTestRepository.java
@@ -0,0 +1,7 @@
+package com.kt.springreportingservice.report.repository;
+
+import com.kt.springreportingservice.report.domain.ErrorTestDomain;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ErrorTestRepository extends JpaRepository<ErrorTestDomain, String> {
+}
