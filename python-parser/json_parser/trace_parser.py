9cd7032ac9fc3da8ab7fe1b3738c6f90cbf5bfdd
jihyoun <lee.jihyoun7@gmail.com>
2024-09-27 16:40:25 +0900

    2024.09.27 - log_parser, trace_parser 개발 완료 - 파일에 새로운 라인 추가되었을 때 해당 라인 이후로 읽어오기 - key_store에 retry, 0으로 수정 - main.py에서 스레드별로 실행하는 부분 확인 필요 (현재는 코드 내에서 while true로 실행)

--- python-parser/json_parser/trace_parser.py
+++ python-parser/json_parser/trace_parser.py
@@ -16,11 +16,13 @@ class TraceParsing:
         file_name = self.file_name
 
         global filter_last_position, original_last_position
-
         parsing_trace_data_list = [] # "a111", "b111"
+
         with open(input_path + file_name, "r") as log_file:
-            # logging.info(last_position)
-            if file_name == "filtered_logs.json":
+            logging.info("filter_last_position")
+            logging.info(filter_last_position)
+
+            if file_name == "filtered_span.json":
                 # 파일 포인터를 마지막 읽은 위치로 이동
                 log_file.seek(filter_last_position)
             else:
@@ -28,21 +30,15 @@ class TraceParsing:
 
             while True:
                 line = log_file.readline()  # 한 줄씩 읽기
-                # logging.info("===========================")
-                # logging.info(line)
+                logging.info("===========================")
+                logging.info(line)
 
                 if not line:  # 더 이상 읽을 데이터가 없으면
                     logging.info("데이터가 없습니다")
                     break  # 루프를 종료
 
-                # # 디버깅할 때 사용..
-                # logging.info('* 아무 글자나 입력:')
-                # input()
                 else:
                     logging.info(f"================ filtered_span 파싱 start: {datetime.datetime.now()} ================")
-                    # # 디버깅할 때 사용..
-                    # logging.info('* 아무 글자나 입력:')
-                    # input()
 
                     try:
                         span_data = json.loads(line.strip())
@@ -53,7 +49,7 @@ class TraceParsing:
                             service_code = None
                             os_type = None
 
-                            # if "resource" in resource_log and "attributes" in resource_log["resource"]:
+                            # if "resource" in resource and "attributes" in resource["resource"]:
                             if "resource" in resource and "attributes" in resource["resource"]:
                                 for attribute in resource["resource"]["attributes"]:
                                     if attribute["key"] == "service.name":
@@ -62,7 +58,7 @@ class TraceParsing:
                                         service_code = attribute["value"]["stringValue"]
                                     if attribute["key"] == "os.type":
                                         os_type = attribute["value"]["stringValue"]
-                            # log_parser에서 for scope_log in resource_log.get("scopeLogs", []):
+                            # trace_parser에서 for scope_span in resource.get("scopeSpans", []):
                             for scopeSpan in resource.get("scopeSpans", []):
                                 for span in scopeSpan.get("spans", []):
                                     # trace에서는 scopespan 안에 attribute가 따로 있음
@@ -113,85 +109,92 @@ class TraceParsing:
                     except json.JSONDecodeError as e:
                         logging.ERROR(f"Error parsing line: {e}")
 
-                logging.info("parsing_trace_data_list\n")
-                logging.info(parsing_trace_data_list)
-                logging.info(len(parsing_trace_data_list))
+                    logging.info("parsing_trace_data_list\n")
+                    logging.info(parsing_trace_data_list)
+                    logging.info(len(parsing_trace_data_list))
 
-                # 마지막으로 읽은 위치를 업데이트
-                if file_name == "filtered_span.json":
-                    # 파일 포인터를 마지막 읽은 위치로 이동
-                    filter_last_position = log_file.tell()+2  # 현재 파일 포인터의 위치를 저장
+                    # 마지막으로 읽은 위치를 업데이트
+                    if file_name == "filtered_span.json":
+                        # 파일 포인터를 마지막 읽은 위치로 이동
+                        filter_last_position = log_file.tell()+1  # 현재 파일 포인터의 위치를 저장
 
-                else:
-                    original_last_position = log_file.tell()+2  # 현재 파일 포인터의 위치를 저장
+                    else:
+                        original_last_position = log_file.tell()+1  # 현재 파일 포인터의 위치를 저장
 
-                logging.info("============ trace 파싱 end ===========\n")
-                logging.info(parsing_trace_data_list)
+                    logging.info("============ trace 파싱 end ===========\n")
+                    logging.info(parsing_trace_data_list)
 
                 return parsing_trace_data_list
 
     def redis_insert(self):
 
-        file_name = self.file_name
+        while True:
 
-        logging.info("============ db 삽입 start===========\n")
+            file_name = self.file_name
+            parsing_trace_data_list = self.traceparser()
 
-        # Redis 클라이언트 설정
-        r = redis.Redis(host='100.83.227.59', port=16379, db=1, password='redis1234!')
+            logging.info("**************************")
+            logging.info("parsing_trace_data_list\n")
+            logging.info(parsing_trace_data_list)
+            logging.info("**************************")
 
-        parsing_trace_data_list = self.traceparser()
+            if parsing_trace_data_list != None:
 
-        logging.info("**************************")
-        logging.info("parsing_trace_data_list\n")
-        logging.info(parsing_trace_data_list)
-        logging.info("**************************")        #
-        # for log in parsing_trace_data_list:
-        #     trace_id = log['traceId']
-        #
-        #     # redis에 저장할 key 값 설정
-        #     key_store_key = f"key_store:{trace_id}"
-        #
-        #     if file_name == "filtered_span.json":
-        #         # 해시 키는 traceId로 설정
-        #         hash_key = f"filtered_trace_hash:{trace_id}"
-        #
-        #     else:
-        #         # 해시 키는 traceId로 설정
-        #         hash_key = f"original_trace_hash:{trace_id}"
-        #
-        #     # # 현재 데이터를 JSON 형식으로 변환
-        #     # log_json = json.dumps(log)
-        #
-        #     # parsing_data_log 필드가 존재하는지 확인하고, 없으면 리스트로 초기화
-        #     existing_logs = r.hget(hash_key, 'parsing_data_trace')
-        #     if existing_logs:
-        #         existing_logs_list = json.loads(existing_logs)
-        #     else:
-        #         existing_logs_list = []
-        #
-        #     # 새로운 로그를 리스트에 추가
-        #     existing_logs_list.append(log)
-        #     logging.info("existing_logs")
-        #     logging.info(existing_logs_list)
-        #     # Redis에 업데이트된 리스트 저장 (HSET으로 해시 업데이트)
-        #     r.hset(hash_key, "parsing_data_trace", json.dumps(existing_logs_list))
-        #     r.hset(key_store_key, "retry", "0")
-        #
-        # # Redis에 저장된 데이터 확인 (예시)
-        # for log in parsing_trace_data_list:
-        #
-        #     trace_id = log['traceId']
-        #
-        #     if file_name == "filtered_span.json":
-        #         # 해시 키는 traceId로 설정
-        #         hash_key = f"filtered_trace_hash:{trace_id}"
-        #
-        #     else:
-        #         # 해시 키는 traceId로 설정
-        #         hash_key = f"original_trace_hash:{trace_id}"
-        #
-        #     logging.info(f"Redis Key: {hash_key}")
-        #     logging.info(r.hget(hash_key, "parsing_data_trace"))
-        #     logging.info(r.hget(hash_key, "parsing_data_trace").decode("utf-8"))
-        #
-        #     logging.info("============ db 삽입 end===========\n")
+                logging.info("============ db 삽입 start===========\n")
+
+                # Redis 클라이언트 설정
+                r = redis.Redis(host='100.83.227.59', port=16379, db=1, password='redis1234!')
+
+
+                for log in parsing_trace_data_list:
+                    trace_id = log['traceId']
+
+                    # redis에 저장할 key 값 설정
+                    key_store_key = f"key_store:{trace_id}"
+
+                    if file_name == "filtered_span.json":
+                        # 해시 키는 traceId로 설정
+                        hash_key = f"filtered_trace_hash:{trace_id}"
+
+                    else:
+                        # 해시 키는 traceId로 설정
+                        hash_key = f"original_trace_hash:{trace_id}"
+
+                    # # 현재 데이터를 JSON 형식으로 변환
+                    # log_json = json.dumps(log)
+
+                    # parsing_data_log 필드가 존재하는지 확인하고, 없으면 리스트로 초기화
+                    existing_logs = r.hget(hash_key, 'parsing_data_trace')
+                    if existing_logs:
+                        existing_logs_list = json.loads(existing_logs)
+                    else:
+                        existing_logs_list = []
+
+                    # 새로운 로그를 리스트에 추가
+                    existing_logs_list.append(log)
+                    logging.info("existing_logs")
+                    logging.info(existing_logs_list)
+
+                    # Redis에 업데이트된 리스트 저장 (HSET으로 해시 업데이트)
+                    r.hset(hash_key, "parsing_data_trace", json.dumps(existing_logs_list))
+                    r.hset(key_store_key, "retry", "0")
+
+                # Redis에 저장된 데이터 확인 (예시)
+                for log in parsing_trace_data_list:
+
+                    trace_id = log['traceId']
+
+                    if file_name == "filtered_span.json":
+                        # 해시 키는 traceId로 설정
+                        hash_key = f"filtered_trace_hash:{trace_id}"
+
+                    else:
+                        # 해시 키는 traceId로 설정
+                        hash_key = f"original_trace_hash:{trace_id}"
+
+                    logging.info(f"Redis Key: {hash_key}")
+                    logging.info(r.hget(hash_key, "parsing_data_trace"))
+                    logging.info(r.hget(hash_key, "parsing_data_trace").decode("utf-8"))
+                    logging.info("\n")
+
+                logging.info("============ db 삽입 end===========\n")
--- python-parser/main.py
+++ python-parser/main.py
@@ -1,32 +1,27 @@
 import threading
 from batch import parser_batch, api_batch, complete_data_batch
-import logging
+import logging, time
 from util.logging_config import setup_logging
 
 setup_logging()
 logging.info("**************** main.py start ****************")
 
+
+thread1 = threading.Thread(target=parser_batch.filtered_log_parsing.redis_insert)  # 각 batch를 실행할 스레드를 생성
+thread2 = threading.Thread(target=parser_batch.original_log_parsing.redis_insert)  # 각 batch를 실행할 스레드를 생성
+thread3 = threading.Thread(target=parser_batch.filtered_trace_parsing.redis_insert)  # 각 batch를 실행할 스레드를 생성
+thread4 = threading.Thread(target=parser_batch.original_trace_parsing.redis_insert)  # 각 batch를 실행할 스레드를 생성
+
 # thread1 = threading.Thread(target=parser_batch.filtered_log_parsing.logparser)  # 각 batch를 실행할 스레드를 생성
-# thread1 = threading.Thread(target=parser_batch.filtered_log_parsing.logparser)  # 각 batch를 실행할 스레드를 생성
-# thread2 = threading.Thread(target=parser_batch.original_log_parsing.logparser)  # 각 batch를 실행할 스레드를 생성
 # thread2 = threading.Thread(target=api_batch.main)
-thread3 = threading.Thread(target=complete_data_batch.main)
+# thread3 = threading.Thread(target=complete_data_batch.main)
 
-# thread1.start()  # 스레드를 시작 (동시에 실행)
-# thread1.start()
-# thread2.start()
+thread1.start()
+thread2.start()
 thread3.start()
+thread4.start()
 
-# thread1.join()  # 스레드가 종료될 때까지 대기
-# thread1.join()
-# thread2.join()
+thread1.join() # 스레드가 종료될 때까지 대기
+thread2.join()
 thread3.join()
-
-
-# while True:
-#     parser_batch.filtered_log_parsing.logparser()
-#     parser_batch.original_log_parsing.logparser()
-#     parser_batch.filtered_trace_parsing.traceparser()
-#     parser_batch.original_trace_parsing.traceparser()
-#
-#     time.sleep(1)
+thread4.join()
