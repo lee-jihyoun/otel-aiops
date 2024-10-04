dbab1d0ed1c25b4d347b9204bab39717e1c506ed
YH <49009854+100yeony@users.noreply.github.com>
2024-10-04 13:50:21 +0900

    2024.09.30 - (fix) complete_data_batch.py 리팩토링 : key_store(hash -> list로 타입 변경) & retry_count_store 사용

--- python-parser/batch/complete_data_batch.py
+++ python-parser/batch/complete_data_batch.py
@@ -15,25 +15,10 @@ def get_redis_db_connection():
     return conn
 
 
-def get_list_key(r, list_info):
-    key_list = r.keys(list_info)
-    print("(key 정보) key_store에 있는 key는", key_list)
-    return key_list
-
-
-def get_only_key(r, key_info):
-    key_ = r.keys(key_info)
-    # key_store:a123 형태에서 a123만을 가져옴
-    key_list = [key.split(":")[1] for key in key_]
-    print("(key 정보)", key_info, "에 있는 key는", key_list)
-    return key_list
-
 def get_parsing_data(r, key_info, key):
     key_ = key_info + ":" + key
-
     # # list 형식일 때
     parsing_data_list = r.lrange(key_, 0, -1)
-    # parsing_data_list = [item.decode('utf-8') for item in parsing_data_list]
     print("*", key_, "의 파싱 데이터:", parsing_data_list, "\n")
     return parsing_data_list
 
@@ -47,7 +32,10 @@ def get_parsing_data(r, key_info, key):
 
 def is_retry_over_2(r, key):
     print("(조건) retry가 2 이상인가?")
-    retry = int(r.hget(key, "retry"))
+    # retry_count_store에 저장된 key의 retry 필드 값을 1 증가(retry 초기값은 0)
+    full_key = "retry_count_store:" + key
+    r.hincrby(full_key, "retry", 1)
+    retry = int(r.hget(full_key, "retry"))
     if retry >= 2:
         print("(결과) yes. retry는", retry)
         return True
@@ -81,54 +69,46 @@ def add_complete_hash(r, key, log, trace):
 
 def main():
     r = get_redis_db_connection()
+    key_store_list = r.lrange("key_store", 0, -1)
     while True:
-        # key 조회
-        key_list = get_list_key(r, "key_store*")
-        filtered_log_key_list = get_only_key(r, "filtered_log_list*")
-        filtered_trace_key_list = get_only_key(r, "filtered_trace_list*")
-
-        for full_key in key_list:
-            key = full_key.split(":")[1]
+        # key_store 리스트에서 key 꺼내기
+        for key in key_store_list:
             print("\n-------------- 현재 key(", key, ")가 포함된 hash 정보 --------------")
             filtered_log = get_parsing_data(r, "filtered_log_list", key)
             filtered_trace = get_parsing_data(r, "filtered_trace_list", key)
 
-            # key의 retry 필드 값을 1 증가(retry 초기값은 0)
-            r.hincrby(full_key, "retry", 1)
-
-            if key in filtered_log_key_list and key in filtered_trace_key_list:
+            # filtered_log, filtered_trace 둘다 존재
+            if len(filtered_log) > 0 and len(filtered_trace) > 0:
                 print("\n(조건) filtered_log_list, filtered_trace_lst에 모두 key가 있는가?")
                 print("(결과) yes\n")
-                result = is_retry_over_2(r, full_key)
+                result = is_retry_over_2(r, key)
                 if result:
                     add_complete_hash(r, key, filtered_log, filtered_trace)
 
-            elif key not in filtered_log_key_list and key in filtered_trace_key_list:
+            elif len(filtered_log) == 0 and len(filtered_trace) > 0:
                 print("(조건) filtered_log_list에는 키가 없고, filtered_trace_list에는 키가 있는가?")
                 print("(결과) yes\n")
-                original_log_key_list = get_only_key(r, "original_log_list*")
-                if key in original_log_key_list:
+                original_log = get_parsing_data(r, "original_log_list", key)
+                if len(original_log) > 0:
                     print("(조건) original_log_hash에 키가 있는가?")
                     print("(결과) yes\n")
-                    result = is_retry_over_2(r, full_key)
+                    result = is_retry_over_2(r, key)
                     if result:
-                        original_log = get_parsing_data(r, "original_log_list", key)
                         add_complete_hash(r, key, original_log, filtered_trace)
                 else:
                     print("(조건) original_log_hash에 키가 있는가?")
                     print("(결과) no\n")
                     continue
 
-            elif key in filtered_log_key_list and key not in filtered_trace_key_list:
+            elif len(filtered_log) > 0 and len(filtered_trace) == 0:
                 print("(조건) filtered_log_list에는 키가 있고, filtered_trace_list에는 키가 없는가?")
                 print("(결과) yes")
-                original_trace_key_list = get_only_key(r, "original_trace_list*")
-                if key in original_trace_key_list:
+                original_trace = get_parsing_data(r, "original_trace_list", key)
+                if len(original_trace) > 0:
                     print("(조건) original_trace_hash에 키가 있는가?")
                     print("(결과) yes\n")
-                    result = is_retry_over_2(r, full_key)
+                    result = is_retry_over_2(r, key)
                     if result:
-                        original_trace = get_parsing_data(r, "original_trace_list", key)
                         add_complete_hash(r, key, filtered_log, original_trace)
                 else:
                     print("(조건) original_trace_hash에 키가 있는가?")
