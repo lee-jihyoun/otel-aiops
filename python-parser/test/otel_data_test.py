64d39056eaa848e6a269062ffc1634487bb22f51
YH <49009854+100yeony@users.noreply.github.com>
2024-10-07 17:30:08 +0900

    2024.09.30 - (fix) otel data test 코드 함수화

--- python-parser/test/otel_data_test.py
+++ python-parser/test/otel_data_test.py
@@ -33,7 +33,7 @@ def parsing_log():
         os.makedirs(output_path)
 
     # 로그 데이터 파싱 및 필요한 key 값 추출
-    filtered_logs = []
+    parsing_log_data_list = []
 
     with open(log_file_path, "r") as log_file:
         for line in log_file:
@@ -42,41 +42,62 @@ def parsing_log():
                 change_timenano_format(log_data)  # 시간 전처리 적용
 
                 for resource_log in log_data.get('resourceLogs', []):
-                    parsed_log = {
-                        "service.name": None,
-                        "service.code": None,
-                        "traceId": None,
-                        "observedTimeUnixNano": None,  # 새로운 시간 필드 추가
-                    }
-
-                    # resource 부분에서 필요한 정보 추출
-                    if "resource" in resource_log and "attributes" in resource_log["resource"]:
-                        for attribute in resource_log["resource"]["attributes"]:
-                            if attribute["key"] == "service.name":
-                                parsed_log["service.name"] = attribute["value"]["stringValue"]
-                            if attribute["key"] == "service.code":
-                                parsed_log["service.code"] = attribute["value"]["stringValue"]
-
-                    # scopeLogs와 logRecords에서 필요한 정보 추출
                     for scope_log in resource_log.get("scopeLogs", []):
                         for log_record in scope_log.get("logRecords", []):
+                            parsed_info = {
+                                "container.id": None,
+                                "os.description": None,
+                                "process.command_line": None,
+                                "service.name": None,
+                                "service.code": None,
+                                "telemetry.sdk.language": None,
+                                "logRecords_severityText": None,
+                                "logRecords_body_stringValue": None,
+                                "flags": None,
+                                "traceId": None,
+                                "observedTimeUnixNano": None,  # 새로운 시간 필드 추가
+                            }
+                            if "resource" in resource_log and "attributes" in resource_log["resource"]:
+                                for attribute in resource_log["resource"]["attributes"]:
+                                    if attribute["key"] == "container.id":
+                                        parsed_info["container.id"] = attribute["value"]["stringValue"]
+                                    if attribute["key"] == "os.description":
+                                        parsed_info["os.description"] = attribute["value"]["stringValue"]
+                                    if attribute["key"] == "process.command_line":
+                                        parsed_info["process.command_line"] = attribute["value"]["stringValue"]
+                                    if attribute["key"] == "service.name":
+                                        parsed_info["service.name"] = attribute["value"]["stringValue"]
+                                    if attribute["key"] == "service.code":
+                                        parsed_info["service.code"] = attribute["value"]["stringValue"]
+                                    if attribute["key"] == "telemetry.sdk.language":
+                                        parsed_info["telemetry.sdk.language"] = attribute["value"]["stringValue"]
+
                             if "observedTimeUnixNano" in log_record:
-                                parsed_log["observedTimeUnixNano"] = log_record["observedTimeUnixNano"]
-                            if "traceId" in log_record and log_record["traceId"] != "":
-                                parsed_log["traceId"] = log_record["traceId"]
-                                # traceId가 유효한 경우에만 logRecord 추가
-                                filtered_logs.append(parsed_log)
+                                parsed_info["observedTimeUnixNano"] = log_record["observedTimeUnixNano"]
+                            if "severityText" in log_record:
+                                parsed_info["logRecords_severityText"] = log_record["severityText"]
+                            if "body" in log_record and "stringValue" in log_record["body"]:
+                                parsed_info["logRecords_body_stringValue"] = log_record["body"]["stringValue"]
+                            if "flags" in log_record:
+                                parsed_info["flags"] = log_record["flags"]
+                            if "traceId" in log_record:
+                                parsed_info["traceId"] = log_record["traceId"]
+                                # logging.info(parsed_info)
+
+                            # traceid가 비어 있을 경우에는 log_record에 저장하지 않음
+                            if "traceId" in log_record and log_record["traceId"]:
+                                parsing_log_data_list.append(parsed_info)
 
             except json.JSONDecodeError as e:
                 print(f"Error parsing line: {e}")
 
     # 로그 데이터를 파일에 저장 (한 줄)
     with open(output_path + log_file_name_one_row, 'w') as log_output_file:
-        json.dump(filtered_logs, log_output_file, separators=(',', ':'))
+        json.dump(parsing_log_data_list, log_output_file, separators=(',', ':'))
 
     # 로그 데이터를 파일에 저장 (여러 줄)
     with open(output_path + log_file_name_multi_row, 'w') as log_output_file:
-        json.dump(filtered_logs, log_output_file, indent=4)
+        json.dump(parsing_log_data_list, log_output_file, indent=4)
 
 
 def parsing_trace():
@@ -100,6 +121,8 @@ def parsing_trace():
                                 service_name = attribute["value"]["stringValue"]
                             if attribute["key"] == "service.code":
                                 service_code = attribute["value"]["stringValue"]
+                            if attribute["key"] == "os.type":
+                                os_type = attribute["value"]["stringValue"]
 
                     for scopeSpan in resource.get("scopeSpans", []):
                         for span in scopeSpan.get("spans", []):
@@ -108,24 +131,44 @@ def parsing_trace():
                                 parsed_info = {
                                     "service.name": service_name,
                                     "service.code": service_code,
+                                    "os.type": os_type,
                                     "traceId": span.get("traceId"),
                                     "spanId": span.get("spanId"),
-                                    # "exception.stacktrace": None,
+                                    "flags": span.get("flags"),
+                                    "name": span.get("name"),
+                                    "http.status_code": None,
+                                    "rpc.grpc.status_code": None,
+                                    "exception.message": None,
+                                    "exception.stacktrace": None,
                                     "exception.stacktrace.short": None,
+                                    "http.url": None,
+                                    "rpc.method": None,
                                     "startTimeUnixNano": span.get("startTimeUnixNano"),
                                     "endTimeUnixNano": span.get("endTimeUnixNano")
                                 }
 
-                            # event 발생 시 event key 내에 exception.message와 exception.stacktrace가 따로 있음
+                                # if span["key"] == "flags":
+                                #     parsed_info["flags"] = span.get("flags")
+                                if attribute["key"] == "http.status_code" and "intValue" in attribute["value"]:
+                                    parsed_info["http.status_code"] = attribute["value"]["intValue"]
+                                if attribute["key"] == "rpc.grpc.status_code":
+                                    parsed_info["rpc.grpc.status_code"] = attribute["value"]["intValue"]
+                                if attribute["key"] == "http.url":
+                                    parsed_info["http.url"] = attribute["value"]["stringValue"]
+                                if attribute["key"] == "rpc.method":
+                                    parsed_info["rpc.method"] = attribute["value"]["stringValue"]
+
+                                # event 발생 시 event key 내에 exception.message와 exception.stacktrace가 따로 있음
                             for event in span.get("events", []):
                                 for attribute in event.get("attributes", []):
+                                    if attribute["key"] == "exception.message":
+                                        parsed_info["exception.message"] = attribute["value"]["stringValue"]
                                     if attribute["key"] == "exception.stacktrace":
-                                        # parsed_info["exception.stacktrace"] = attribute["value"]["stringValue"]
+                                        parsed_info["exception.stacktrace"] = attribute["value"]["stringValue"]
                                         # 두 번째 \n 전까지만 가져오기, 중간에 \n 존재 시 삭제
-                                        parsed_info["exception.stacktrace.short"] = ' '.join(
-                                            line.strip() for line in attribute["value"]["stringValue"].split('\n')[:2])
+                                        parsed_info["exception.stacktrace.short"] = ' '.join(line.strip() for line in attribute["value"]["stringValue"].split('\n')[:2])
 
-                                filtered_spans.append(parsed_info)
+                            filtered_spans.append(parsed_info)
 
             except json.JSONDecodeError as e:
                 print(f"Error parsing line: {e}")
